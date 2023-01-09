2dc0dde415734621ef953c58d0e5e5916be79d95
Abel Herrera <ahpard97@gmail.com>
2023-01-08 21:25:50 -0600

    add test for second example

--- countIntersections.test.js
+++ countIntersections.test.js
@@ -10,4 +10,14 @@ describe('countIntersections test suite', () => {
 
       expect(result).toEqual(expectedIntersections);
    });
+
+   it('should calculate correctly the intersections of segments from second example', () => {
+      const startsAt = [1, 3];
+      const endsAt = [5, 4];
+      const expectedIntersections = [1, 1];
+
+      const result = countIntersections(startsAt, endsAt);
+
+      expect(result).toEqual(expectedIntersections);
+   });
 });
